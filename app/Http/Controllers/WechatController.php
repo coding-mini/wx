34d07c694b6617c0a9e5738ee429dc14143d9884
Codinget <codinget@163.com>
2019-01-28 16:57:16 +0800

    添加微信开发包

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -14,52 +14,10 @@ class WechatController extends Controller
      */
     public function serve()
     {
-        Log::info('I am wechat server');
-
         $wechat = app('wechat.official_account');
 
-        $wechat->server->push(function($message) use($wechat){
-
-            $user = $wechat->user->get($message->FromUserName);
-
-            $responseMsg = '';
-
-            switch ($message->MsgType) {
-                case 'event':
-                    switch ($message->Event) {
-                        case 'subscribe':
-                            return '欢迎您关注 Coding10 公众号';
-
-                        case 'CLICK':
-                            switch ($message->EventKey) {
-                                case 'BUTTON_ABOUT_US':
-                                    $user_auth = $wechat->oauth->user();
-                                    return $user_auth->nickname.'你点击了关于我们';
-                                case 'V1001_GOOD':
-                                    return '你点击了赞一下我们';
-                            }
-                            return '';
-                        default:
-                            break;
-                    }
-                    return '';
-                case 'text':   // 文本消息
-                    $responseMsg = $user->nickname.'我是个不会聊天的人';
-                    break;
-                case 'image':
-                    $responseMsg = $user->nickname.'收到图片消息';
-                    break;
-                case 'video':
-                    $responseMsg = $user->nickname.'我非常喜欢做视频';
-                    break;
-                case 'voice':
-                    $responseMsg = $user->nickname.'我非常喜欢做音频';
-                    break;
-                default:
-                    $responseMsg = $user->nickname.'我是没有个性的默认恢复消息';
-                    break;
-            }
-            return $responseMsg;
+        $wechat->server->push(function($message){
+            return "欢迎关注 Coding10！";
         });
 
         return $wechat->server->serve();
