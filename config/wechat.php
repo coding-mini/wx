9ac245c27f0149fd8b9614865b5460971894e828
Codinget <codinget@163.com>
2019-01-28 16:31:19 +0800

    添加微信开发包

--- config/wechat.php
+++ config/wechat.php
@@ -17,7 +17,7 @@ return [
         /*
          * 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
          */
-        'response_type' => 'array',
+        'response_type' => 'object',
 
         /*
          * 使用 Laravel 的缓存系统
