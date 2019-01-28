1f67b4f241c62ec0ff84804a0d51d9a928e37a11
Codinget <codinget@163.com>
2019-01-28 16:27:19 +0800

    添加微信开发包

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -16,25 +16,7 @@ class WechatController extends Controller
         $wechat = app('wechat.official_account');
 
         $wechat->server->push(function($message){
-            $responseMsg = '';
-            switch ($message->MsgType) {
-                case 'text':   // 文本消息
-                    $responseMsg = '我是个不会聊天的人';
-                    break;
-                case 'image':
-                    $responseMsg = '收到图片消息';
-                    break;
-                case 'video':
-                    $responseMsg = '我非常喜欢做视频';
-                    break;
-                case 'voice':
-                    $responseMsg = '我非常喜欢做音频';
-                    break;
-                default:
-                    $responseMsg = '我是没有个性的默认恢复消息';
-                    break;
-            }
-            return $responseMsg;
+            return '';
         });
 
         return $wechat->server->serve();
