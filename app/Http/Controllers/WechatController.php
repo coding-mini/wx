566eee03184d89ef19be56e8245572aba92a230c
Codinget <codinget@163.com>
2019-01-28 17:23:30 +0800

    添加微信开发包

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -16,8 +16,24 @@ class WechatController extends Controller
     {
         $wechat = app('wechat.official_account');
 
-        $wechat->server->push(function($message){
-            return "欢迎关注 Coding10！";
+        $wechat->server->push(function($message) use ($wechat){
+            $user = $wechat->user->get($message->FromUserName);
+            switch ($message['MsgType']) {
+                case 'event':
+                    return $user->nickname.'收到事件消息';
+
+                case 'text':
+                    return $user->nickname.'收到文字消息';
+
+                case 'image':
+                    return $user->nickname.'收到图片消息';
+
+                case 'voice':
+                    return $user->nickname.'收到语音消息';
+
+                default:
+                    return $user->nickname.'收到其它消息';
+            }
         });
 
         return $wechat->server->serve();
--- config/wechat.php
+++ config/wechat.php
@@ -17,7 +17,7 @@ return [
         /*
          * 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
          */
-        'response_type' => 'object',
+        'response_type' => 'array',
 
         /*
          * 使用 Laravel 的缓存系统
