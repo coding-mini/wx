7fc1406b6655b0455f911408cfb49440258cbef4
Codinget <codinget@163.com>
2019-01-29 13:10:27 +0800

    添加微信开发包

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -40,6 +40,9 @@ class WechatController extends Controller
                     return $user->nickname.'收到文字消息';
 
                 case 'image':
+                    $image = new Image('wWzPhXyhPpOxBB-jDIPmwk6FkS_i1qcg74VwcjVjEVo');
+                    return $image;
+                    $wechat->staff->message($image)->to($message->FromUserName)->send();
                     return $user->nickname.'收到图片消息';
 
                 case 'voice':
